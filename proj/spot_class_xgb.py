14c73b0f8de6f174c686aa9a2243bb12add03ac0
abihi <ahmed.bihi@hotmail.com>
2019-02-16 16:05:06 +0100

    gridsearchCV

--- proj/spot_class_xgb.py
+++ proj/spot_class_xgb.py
@@ -16,10 +16,10 @@ dataset = pd.read_csv("training_data.csv")
 testset = pd.read_csv("songs_to_classify.csv")
 
 y = dataset.label
-X = dataset.drop(['label', 'time_signature', 'mode', 'key', 'loudness', 'energy'], axis=1)
-testset = testset.drop(['time_signature', 'mode', 'key', 'loudness', 'energy'], axis=1)
+X = dataset.drop(['label'], axis=1)
+#testset = testset.drop(['time_signature'], axis=1)
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1)
 
 #Visualize data (search for correlations)
 #sns.pairplot(X_train[X_train.columns.values], diag_kind="kde")
@@ -41,29 +41,28 @@ from xgboost import XGBClassifier
 from sklearn import  metrics   #Additional scklearn functions
 from sklearn.model_selection import GridSearchCV, KFold, StratifiedKFold, cross_val_score
 
-def modelfit(alg, dtrain, targets, final_pred, useTrainCV=True, cv_folds=10, early_stopping_rounds=100):
+def modelfit(alg, dtrain, targets, final_pred, useTrainCV=True, cv_folds=10, early_stopping_rounds=1000):
 
     if useTrainCV:
         xgb_param = alg.get_xgb_params()
         xgtrain = xgb.DMatrix(dtrain.values, label=targets.values)
-        cvresult = xgb.cv(xgb_param, xgtrain, num_boost_round=alg.get_params()['n_estimators'], stratified=False,
-            nfold=cv_folds, metrics='auc', early_stopping_rounds=early_stopping_rounds, verbose_eval=None)
+        cvresult = xgb.cv(xgb_param, xgtrain, num_boost_round=alg.get_params()['n_estimators'], stratified=True,
+            nfold=cv_folds, metrics=['auc'], early_stopping_rounds=early_stopping_rounds, verbose_eval=None)
         alg.set_params(n_estimators=cvresult.shape[0])
         #print(cvresult.shape[0])
 
-    eval_set = [(dtrain, targets), (normed_test_data, y_test)]
+    #kfold = KFold(n_splits=10, random_state=7)
+    kfold = StratifiedKFold(n_splits=10, random_state=7)
+    results = cross_val_score(alg, normed_train_data, y_train, cv=kfold)
 
+    eval_set = [(dtrain, targets), (normed_test_data, y_test)]
     #Fit the algorithm on the data
-    alg.fit(dtrain, targets, eval_metric=['auc', 'error'], eval_set=eval_set, verbose=False)
+    alg.fit(dtrain, targets, eval_metric=['auc'], eval_set=eval_set, verbose=False)
 
     #Predict training set:
     dtrain_predictions = alg.predict(dtrain)
     final_predictions = alg.predict(final_pred)
     dtrain_predprob = alg.predict_proba(dtrain)[:,1]
-
-    #kfold = KFold(n_splits=10, random_state=7)
-    kfold = StratifiedKFold(n_splits=10, random_state=7)
-    results = cross_val_score(alg, normed_train_data, y_train, cv=kfold)
 
     prediction_string = ""
     for i in final_predictions:
@@ -78,23 +77,23 @@ def modelfit(alg, dtrain, targets, final_pred, useTrainCV=True, cv_folds=10, ear
     print ("Accuracy(CV): %.2f%% (%.2f%%)" % (results.mean()*100, results.std()*100))
     #https://stats.stackexchange.com/questions/132777/what-does-auc-stand-for-and-what-is-it
 
-    feat_imp = pd.Series(alg.get_booster().get_fscore()).sort_values(ascending=False)
+    feat_imp = pd.Series(alg.get_booster().get_score(importance_type='gain')).sort_values(ascending=False)
     feat_imp.plot(kind='bar', title='Feature Importances')
     plt.ylabel('Feature Importance Score')
-    #plt.show()
+    plt.show()
 
 xgb_model = XGBClassifier(
      learning_rate =0.1,
-     n_estimators=5000,
+     n_estimators=1000,
      max_depth=6,
      min_child_weight=3,
-     gamma=0.2,
+     gamma=0.5,
      subsample=0.45,
      colsample_bytree=0.35,
-     objective= 'binary:logistic',
+     objective= 'binary:hinge',
      nthread=4,
      reg_alpha=0.05,
-     scale_pos_weight=1,
+     scale_pos_weight=0,#1
      seed=27
     )
 modelfit(xgb_model, normed_train_data, y_train, normed_test_final)
@@ -105,8 +104,3 @@ modelfit(xgb_model, normed_train_data, y_train, normed_test_final)
 #Predictions on final test set
 #preds_final = xgb_model.predict(normed_test_final)
 #print(preds_final)
-
-#all_1 = [1]*len(y)
-
-#from sklearn.metrics import accuracy_score
-#print(accuracy_score(y,all_1))
--- proj/spot_class_xgb_org.py
+++ proj/spot_class_xgb_org.py
@@ -16,9 +16,10 @@ dataset = pd.read_csv("training_data.csv")
 testset = pd.read_csv("songs_to_classify.csv")
 
 y = dataset.label
-X = dataset.drop(['label'], axis=1)
+X = dataset.drop(['label', 'time_signature'], axis=1)
+testset = testset.drop(['time_signature'], axis=1)
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0)
 
 #Visualize data (search for correlations)
 #sns.pairplot(X_train[X_train.columns.values], diag_kind="kde")
@@ -39,8 +40,8 @@ normed_test_final = norm(testset)
 #Change model here to your own
 from xgboost import XGBClassifier
 xgb_model = XGBClassifier(
-    learning_rate =0.001,
-    n_estimators=5000,
+    learning_rate =0.1,
+    n_estimators=200,
     max_depth=6,
     min_child_weight=3,
     gamma=0.2,
@@ -61,14 +62,24 @@ for i in final_predictions:
     prediction_string += str(i)
 print(prediction_string)
 
-from sklearn.model_selection import StratifiedKFold
-from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import StratifiedKFold, cross_val_score, GridSearchCV
 
 kfold = StratifiedKFold(n_splits=10, random_state=7)
 results = cross_val_score(xgb_model, normed_train_data, y_train, cv=kfold)
 print("Accuracy (CV): %.2f%% (%.2f%%)" % (results.mean()*100, results.std()*100))
 
+param_test1 = {
+ 'max_depth':range(1,16,1),
+ 'min_child_weight':range(1,10,1)
+}
+gsearch1 = GridSearchCV(estimator = XGBClassifier( learning_rate =0.1, n_estimators=200, max_depth=6,
+ min_child_weight=3, gamma=0, subsample=0.8, colsample_bytree=0.8,
+ objective= 'binary:logistic', nthread=4, scale_pos_weight=1, seed=27),
+ param_grid = param_test1, scoring='accuracy',n_jobs=4,iid=False, cv=5)
+gsearch1.fit(normed_train_data, y_train)
+print("Grid scored: ", gsearch1.cv_results_, "Best params: ", gsearch1.best_params_, "Best score: ", gsearch1.best_score_)
+
 #Predictions on test set
-predictions = xgb_model.predict(normed_test_data)
-from sklearn.metrics import accuracy_score
-print("Accuracy (test set): %.2f%%" % accuracy_score(y_test,predictions))
+#predictions = xgb_model.predict(normed_test_data)
+#from sklearn.metrics import accuracy_score
+#print("Accuracy (test set): %.2f%%" % accuracy_score(y_test,predictions))
